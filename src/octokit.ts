a17c57e87c61780af47abbbe65878e1bb8448bda
George Michael <george.michael@unicaf.org>
2023-11-24 15:57:45 +0200

    node fetch

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,5 +1,6 @@
 import { Octokit } from '@octokit/core';
 import { config } from '@probot/octokit-plugin-config';
+import { fetch } from '@node-fetch';
 
 const CustomOctokit = Octokit.plugin(config);
 
@@ -7,6 +8,9 @@ export type CustomOctokit = InstanceType<typeof CustomOctokit>;
 
 export function getOctokit(token: string) {
   return new CustomOctokit({
+    request: {
+      fetch: node-fetch,
+    },
     auth: token,
     baseUrl: process.env.GITHUB_API_URL ?? 'https://api.github.com',
   });
